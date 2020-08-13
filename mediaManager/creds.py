70959099d4d3880db23f43fdc82547edc92af681
hrk2023 <hrkdasgupta025@gmail.com>
2020-08-14 00:25:46 +0530

    Deploy Attempt 4

--- mediaManager/creds.py
+++ mediaManager/creds.py
@@ -1,5 +1,5 @@
 import os
 from pymongo import MongoClient
 DATABASE_URI = os.environ.get('DATABASE_URI',"default_value")
-cluster = MongoClient(DATABASE)
+cluster = MongoClient(DATABASE_URI)
 db = cluster.eceSite
